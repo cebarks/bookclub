fa3ebf28640636ffd3b5c3b648f46b938d4a0573
Anten Skrabec <cebarks@gmail.com>
2018-12-05 14:57:05 -0700

    adds models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   validates_presence_of :title, :pages, :pub_date
-  has_many :author, through: :book_author
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+  has_many :reviews
 end
