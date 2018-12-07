b0adf251605a6caefd2353510a14870aecc32684
Anten Skrabec <cebarks@gmail.com>
2018-12-07 16:43:37 -0700

    adds test for deleting an author #7

---
+++ spec/features/user_can_delete_an_author_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor' do
+  describe "When I visit an author's show page" do
+    it "theres a link that deletes the author" do
+      author1 = Author.create(name: "Sal")
+      author2 = Author.create(name: "Ian")
+
+      book1 = Book.create(title: "The Future", pages: 200, pub_date: "2/2/2022", authors: [author1])
+      book2 = Book.create(title: "2: The Future", pages: 400, pub_date: "4/4/4044", authors: [author1, author2])
+
+      visit author_path(author1)
+
+      click_link('Delete')
+
+      expect(current_path).to eq(books_path)
+
+      expect(page).to_not have_content(book1.title)
+      expect(page).to_not have_content(book1.pages)
+      expect(page).to_not have_content(book1.pub_date)
+      expect(page).to have_content(book2.title)
+      expect(page).to have_content(book2.pages)
+      expect(page).to have_content(book2.pub_date)
+    end
+  end
+end
