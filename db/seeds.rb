1428989ee5d418162d8714a74ca57c94009b6ad7
Anten Skrabec <cebarks@gmail.com>
2018-12-07 16:45:02 -0700

    add initial seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
+@@ -6,10 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Seed books and authors
+require 'json'
+js = JSON.load(File.new("db/books.json"))
 
 author = Author.create(name:"I am who am")
 book1 = author.books.create(title: "The Bible", pages: 666, pub_date: 50)
@@ -13,3 +12,19 @@ user1 = User.create(username:"99Theses")
 user2 = User.create(username:"JesusLover123")
 review = book1.reviews.create(title:"Twist ending", description: "I didn't see that ressurection thing coming", rating:4, user:user1, book:book1)
 review = book1.reviews.create(title:"Predictable", description: "Totally expected the ending.", rating:2, user:user2, book:book1)
+js.each do |book|
+  author = book['author']
+  author_obj = Author.find_or_create_by(name: author)
+
+  params = {
+    title: book['title'],
+    pages: book['pages'],
+    pub_date: book['year']
+  }
+
+  author_obj.books.create(params)
+
+  rand(1..3).times do
+    book.reviews.create()
+  end
+end
