bca7bbe4b93d061fdae171aa720bc8cfac1d701b
Anten Skrabec <cebarks@gmail.com>
2018-12-10 16:14:01 -0700

    displays the top review for an author on the show page

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,13 @@ class Author < ApplicationRecord
   validates_presence_of :name
   has_many :book_authors, :dependent => :destroy
   has_many :books, through: :book_authors
+
+  def top_review
+    reviews.order(:rating).last
+  end
+
+  def reviews
+    ids = books.pluck(:id)
+    Review.where(book_id: ids).order(:rating)
+  end
 end
