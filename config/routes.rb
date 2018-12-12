bfb304068c16a5fbd57a040957141a81a586f6a6
Anten Skrabec <cebarks@gmail.com>
2018-12-11 23:24:16 -0700

    removed the ability for authors to be editted

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:show, :index, :destroy, :new]
-  resources :authors, only: [:show, :index, :edit, :destroy, :new]
   resources :reviews, only: [:show, :index, :edit, :destroy, :new, :create]
   resources :books, only: [:show, :index, :new, :create, :edit, :destroy, :new]
+  resources :authors, only: [:show, :index, :destroy, :new]
   resources :welcome, only: [:index]
 
   root 'welcome#index'
