b31bae009b9f6c7cfc986af0340e1c6c0a6e5238
Anten Skrabec <cebarks@gmail.com>
2018-12-11 23:15:10 -0700

    restricts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :book_authors
-  resources :authors
-  resources :reviews
-  resources :books
-  resources :welcome
+  resources :users, only: [:show, :index, :destroy, :new]
+  resources :authors, only: [:show, :index, :edit, :destroy, :new]
+  resources :reviews, only: [:show, :index, :edit, :destroy, :new, :create]
+  resources :books, only: [:show, :index, :new, :create, :edit, :destroy, :new]
+  resources :welcome, only: [:index]
 
   root 'welcome#index'
 end
